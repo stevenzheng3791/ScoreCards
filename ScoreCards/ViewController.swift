45dc7090c63429b7fb111cb88d6bb6a8e97d595c
Steven Zheng <stevenzheng3791@gmail.com>
2017-07-13 21:20:28 -0400

    Initial Commit

---
+++ ScoreCards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScoreCards
+//
+//  Created by Steven Zheng on 7/13/17.
+//  Copyright © 2017 szzheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
